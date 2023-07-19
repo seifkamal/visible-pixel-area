85e24c9b2d18bfe0c2ab48abe12294da4049c1de
Seif Kamal <seifskm@gmail.com>
2023-07-19 15:22:42 +0100

    feat: Make blue shapes drag and droppable

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+import { init } from "./src/app.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("let's go");
+  const blueShapeNodes = document.querySelectorAll(".blue");
+  const blueShapes = Array.from(blueShapeNodes).map((shape) => {
+    if (!(shape instanceof HTMLElement)) {
+      throw new Error(".blue must be an HTMLElement");
+    }
+    return shape;
+  });
+
+  const dropContainer = document.querySelector("main");
+  if (!(dropContainer instanceof HTMLElement)) {
+    throw new Error("main drop area must be an HTMLElement");
+  }
+
+  init({
+    blueShapes,
+    dropContainer,
+  });
 });
---
+++ src/app.js
@@ -0,0 +1,83 @@
+export const events = new EventTarget();
+export const eventTypes = {
+  shapeDrop: "app:shape:drop",
+};
+
+/**
+ * @typedef {{
+ *   blueShapes: HTMLElement[];
+ *   dropContainer: HTMLElement;
+ * }} AppProps
+ *
+ * @param {AppProps} props
+ */
+export function init({ blueShapes, dropContainer }) {
+  initBlueShapes(blueShapes);
+  initDropContainer(dropContainer);
+}
+
+/**
+ * @param {AppProps['blueShapes']} blues
+ */
+function initBlueShapes(blues) {
+  // Limit the space available for the spawn position randomising.
+  const spawnXOffset = window.innerWidth / 3;
+  const spawnYOffset = window.innerHeight / 1.5;
+
+  blues.forEach((blue, idx) => {
+    if (!blue.id) {
+      blue.id = `shape-${idx}`;
+    }
+
+    const randomX = Math.random() * spawnXOffset;
+    const randomY = Math.random() * spawnYOffset;
+    blue.style.left = `${randomX}px`;
+    blue.style.top = `${randomY}px`;
+    blue.draggable = true;
+
+    blue.addEventListener("dragstart", (event) => {
+      if (!event.dataTransfer) {
+        return;
+      }
+
+      const data = {
+        id: blue.id,
+        offsetX: event.offsetX,
+        offsetY: event.offsetY,
+      };
+
+      event.dataTransfer.setData("text/plain", JSON.stringify(data));
+      blue.toggleAttribute("data-dragging", true);
+    });
+
+    blue.addEventListener("dragend", () => {
+      blue.toggleAttribute("data-dragging", false);
+    });
+  });
+}
+
+/**
+ * @param {AppProps['dropContainer']} container
+ */
+function initDropContainer(container) {
+  container.addEventListener("dragover", (event) => event.preventDefault());
+  container.addEventListener("drop", async (event) => {
+    const draggableData = event.dataTransfer?.getData("text/plain");
+    if (!draggableData) {
+      return;
+    }
+
+    const { id, offsetX, offsetY } = JSON.parse(draggableData);
+    const draggable = document.getElementById(id);
+    if (!draggable) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const { clientX: x, clientY: y } = event;
+    draggable.style.left = `${x - offsetX}px`;
+    draggable.style.top = `${y - offsetY}px`;
+    events.dispatchEvent(new Event(eventTypes.shapeDrop));
+  });
+}
